3ac0b0b9f6db660d903d0cf0f4e103cff2ea46f8
Piyush Panchal <piyushpanchal114@gmail.com>
2025-04-06 17:29:21 +0530

    session manager

---
+++ app/db.py
@@ -0,0 +1,55 @@
+import contextlib
+import os
+from typing import Any, AsyncIterator
+from sqlalchemy.ext.asyncio import (create_async_engine,
+                                    async_sessionmaker, AsyncSession)
+
+
+class DatabaseSessionManager:
+    """Database session manager"""
+
+    def __init__(self, host: str, engine_kwargs: dict[str, Any] = {}):
+        self._engine = create_async_engine(host, **engine_kwargs)
+        self._sessionmaker = async_sessionmaker(
+            bind=self._engine, expire_on_commit=False)
+
+    async def close(self):
+        if self._engine is None:
+            raise Exception("Database session manager is not initialized")
+        await self._engine.dispose()
+
+        self._engine = None
+        self._sessionmaker = None
+
+    @contextlib.asynccontextmanager
+    async def connect(self) -> AsyncIterator[AsyncSession]:
+        if self._engine is None:
+            raise Exception("Database session manager is not initialized")
+        async with self._engine.begin() as conn:
+            try:
+                yield conn
+            except Exception:
+                await conn.rollback()
+                raise
+
+    @contextlib.asynccontextmanager
+    async def session(self) -> AsyncIterator[AsyncSession]:
+        if self._sessionmaker is None:
+            raise Exception("Database session manager is not initialized")
+        async with self._sessionmaker() as session:
+            try:
+                yield session
+            except Exception:
+                await session.rollback()
+                raise
+
+
+db_url = os.environ.get("DB_URL")
+echo_sql = bool(int(os.environ.get("ECHO_SQL", 0)))
+
+sessionmanager = DatabaseSessionManager(db_url, {"echo": echo_sql})
+
+
+async def get_session() -> AsyncIterator[AsyncSession]:
+    async with sessionmanager.session() as session:
+        yield session

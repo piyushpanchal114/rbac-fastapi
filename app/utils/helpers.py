f4fce9b0f7a5888cc7f4af52ea4402c624ae700e
Piyush Panchal <piyushpanchal114@gmail.com>
2025-04-10 16:35:45 +0530

    removed print statement

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -5,7 +5,5 @@ from models.db_models import User
 
 async def get_user(user_id: int, db: AsyncSession) -> User | None:
     """Function for getting user by id"""
-    print("db", db)
     qs = await db.scalars(select(User).where(User.id == user_id))
-    print("qs", qs)
     return qs.first()

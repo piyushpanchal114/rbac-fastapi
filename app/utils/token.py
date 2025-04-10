91707d231e8c4c0f0ca1c342b7d25b3dacb1b560
Piyush Panchal <piyushpanchal114@gmail.com>
2025-04-10 15:31:29 +0530

    jwt access and refresh token

--- app/utils/token.py
+++ app/utils/token.py
@@ -1,16 +1,24 @@
 import jwt
+import uuid
 from datetime import datetime, timedelta, timezone
+from typing import Literal
 from config import settings
 
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    """Function for creating access token"""
+def create_jwt_token(data: dict, type: Literal["refresh", "access"],
+                     expires_delta: timedelta | None = None):
+    """Function for creating jwt token"""
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
+    elif type == "refresh":
+        expire = datetime.now(timezone.utc) + timedelta(days=30)
     else:
         expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, settings.secret,
+    created_at = datetime.now(timezone.utc)
+    payload = {"token_type": type, "exp": expire,
+               "iat": created_at, "jti": str(uuid.uuid4())}
+    payload.update(**to_encode)
+    encoded_jwt = jwt.encode(payload, settings.secret,
                              algorithm=settings.algorithm)
     return encoded_jwt

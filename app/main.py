215ddb49a60ad1e46a37f22077cce9d9d349a5f8
Piyush Panchal <piyushpanchal114@gmail.com>
2025-04-19 13:33:32 +0530

    sort the api docs

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ from controllers import users_api, auth_api
 
 app = FastAPI()
 
-app.include_router(users_api.router)
 app.include_router(auth_api.router)
+app.include_router(users_api.router)
 
 
 @app.get("/ping")

ae199bc5877d108c712cba2becb0ed187025926a
Piyush Panchal <piyushpanchal114@gmail.com>
2025-04-19 13:56:53 +0530

    get user details

--- app/controllers/users_api.py
+++ app/controllers/users_api.py
@@ -1,11 +1,12 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from db import get_session
 from schemas import users
 from models.db_models import User
 from models.enums import UserRoleEnum
+from utils.helpers import get_user
 from utils.permissions import RoleChecker
 
 router = APIRouter(
@@ -22,3 +23,15 @@ async def get_all_users(
     """Get All Users"""
     users = await session.scalars(select(User).order_by(User.created_at))
     return users
+
+
+@router.get("/{user_id}")
+async def get_user_details(
+     _: Annotated[bool, Depends(
+         RoleChecker(allowed_roles=[UserRoleEnum.admin]))],
+     user_id: int, session: AsyncSession = Depends(get_session)) -> users.User:
+    user = await get_user(user_id=user_id, db=session)
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="User with user id does not exists.")
+    return user
